cefaf4f661447214803f78f61a704707246a97e9
Rafael Keramidas <rafael@keramid.as>
2022-10-26 13:25:40 +0200

    update code

--- app2/index.js
+++ app2/index.js
@@ -1,24 +1,12 @@
-const fs = require('fs');
 const path = require('path');
-const https = require('https');
 const express = require('express');
 
 const app = express();
 const port = 8000;
-const options = {
-    key: fs.readFileSync(__dirname + '/../selfsigned.key'),
-    cert: fs.readFileSync(__dirname + '/../selfsigned.crt')
-};
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
-
-/*
-https.createServer(options, app).listen(port, () => {
-    console.log(`App 2 listening on port ${port}`);
-});
-*/
 
 app.listen(port, () => {
     console.log(`App 2 listening on port ${port}`);
